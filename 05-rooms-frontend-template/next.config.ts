bfa74d925ba528adfe986c5cbdf2f791959a91b2
Jan Amann <jan@amann.work>
2026-03-24 10:44:26 +0100

    Update `next.config.ts` of `05-rooms-frontend-template`

--- 05-rooms-frontend-template/next.config.ts
+++ 05-rooms-frontend-template/next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [{hostname: 'i.pravatar.cc'}, {hostname: 'c.pxhere.com'}]
+  },
+  logging: {
+    fetches: {
+      fullUrl: true
+    }
+  }
 };
 
 export default nextConfig;
